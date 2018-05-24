600efac5d9e080158e867d2a5328837f2c4a70b7
Peter GL Huang <peter_gl_huang@wistron.com>
2018-05-24 16:27:31 +0800

    first commit

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=annbot-bot-Web-Deploy.pubxml -p:Password=sxZKsXKXBcxFgp5RoWacfhFwdb3wzZxT9BYkHqGk3Z3PbQx8nZffbrDnCQkh
+
